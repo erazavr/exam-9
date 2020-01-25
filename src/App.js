19d3ac2b43b357ce26e2410821ea4aa83de90c6b
Ernur Kurmanbekov <erazavr85@gmail.com>
2020-01-25 16:53:27 +0600

    Добавил новый Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {Route, Switch} from "react-router";
 import ContactsPage from "./containers/ContactsPage/ContactsPage";
+import Layout from "./components/Layout/Layout";
+import NewContact from "./containers/NewContact/NewContact";
+import EditContact from "./containers/EditContact/EditContact";
 
 const App = () => (
-    <Switch>
-      <Route path='/' exact component={ContactsPage}/>
-    </Switch>
+    <Layout>
+        <Switch>
+            <Route path='/' exact component={ContactsPage}/>
+            <Route path='/newContact' exact component={NewContact}/>
+            <Route path='/editContact/:id' exact component={EditContact}/>
+        </Switch>
+    </Layout>
+
 );
 
 export default App;
